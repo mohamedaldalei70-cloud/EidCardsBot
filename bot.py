d5168050bf806ee7ad2b4be2cc66c40af82f9930
mohamedaldalei70-cloud <mohamedaldalei70@gmail.com>
2026-05-27 03:54:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "🎉 أهلاً بك في بوت بطاقات تهاني العيد\n\nاكتب اسمك لإنشاء بطاقة العيد"
     )
 
-app = Application.builder().token(TOKEN).build()
+def main():
+    app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        CommandHandler("start", start)
+    )
 
-app.run_polling()
+    print("Bot started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
